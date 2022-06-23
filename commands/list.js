154119bfff32ab0e164f58c0ea870ac9ebcc6d25
stefanitos <65655225+stefanitos@users.noreply.github.com>
2022-06-23 15:36:42 +0200

    s

--- commands/list.js
+++ commands/list.js
@@ -29,7 +29,7 @@ module.exports = {
                     const anime_list = result.anime_list;
                     let anime_list_string = "";
                     const nLen = anime_list.length;
-                    if (nLen == 0) userTarget ? interaction.reply(`${name}'s anime list is empty.`) : interaction.reply("Your anime list is empty.");
+                    if (nLen == 0) userTarget ? return interaction.reply(`${name}'s anime list is empty.`) : return interaction.reply("Your anime list is empty.");
                     for (let i = 0; i < nLen; i++) {
                         anime_list_string += anime_list[i] + "\n";
                     }
